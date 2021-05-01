aba636bbfd0cf62af9010fe2d9cc79d41868d39f
LVignoni <leandrovignoni@gmail.com>
2021-05-01 20:39:02 -0300

    oppacity

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function transition() {
   var tl = gsap.timeline();
-  tl.to("ul.transition li", { duration: 0.5, scaleY: 1, transformOrigin: "bottom left", stagger: 0.2 });
+  tl.to("ul.transition li", { duration: 0.5, scaleY: 1, transformOrigin: "bottom left", stagger: 0.2 }, 0);
   tl.to("ul.transition li", { duration: 0.5, scaleY: 0, transformOrigin: "bottom left", stagger: 0.1, delay: 0.1 });
+  tl.to(".left", { opacity: 0, duration: 0.5 }, 0)
 }
 
 function animation() {
   var tl = gsap.timeline();
   tl.from(".left", { duration: 1, translateY: 50, opacity: 0 });
-  tl.to("img", { clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)", duration: 1 }, "-=1");
+  tl.to("img", { clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)", duration: 1 }, "-=0.8");
 }
 
 function delay(n) {
